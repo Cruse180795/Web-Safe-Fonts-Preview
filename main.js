2131bbba2b67b21659ebfcb49fd849de7a274847
ryan <ryancruse1807@gmail.com>
2022-06-14 16:43:59 +0100

    Project Solution

--- main.js
+++ main.js
@@ -1,3 +1,52 @@
 $(document).ready(() =>{
-    alert('Page loaded')
+
+    const $preview = $('.preview');
+    
+    function UpdateTextPreview(){
+        const $text = $('#text');
+
+        $text.on('keyup', (event) =>{
+            $currentText = $(event.currentTarget).val();
+            $preview.html($currentText);
+    });
+    }
+
+    function changeFontFamily(){
+        const $font = $('#font');
+
+        $font.on('change', (event) =>{
+            const $fontFamily = $(event.currentTarget).val();
+            $preview.css({
+            fontFamily: $fontFamily
+            });
+        });
+    }
+
+    function changeFontWeight(){
+        const $weight = $('#weight');
+
+        $weight.on('change', (event) =>{
+            const $fontWeight = $(event.currentTarget).val();
+            $preview.css({
+                fontWeight: $fontWeight
+            });
+        });
+    }
+
+    function changeFontSize(){
+        const $size = $('#size');
+        $size.on('keyup', (event) =>{
+            const $fontSize = $(event.currentTarget).val();
+
+            $preview.css({
+                fontSize: $fontSize + "px"
+            })
+        })
+    }
+
+    UpdateTextPreview();
+    changeFontFamily();
+    changeFontWeight();
+    changeFontSize();
+    
 })
